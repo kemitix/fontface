9020950d9016c268b4e4f65c1d1ce95296827446
Paul Campbell <pcampbell@kemitix.net>
2020-09-18 18:39:47 +0100

    Merge FontLoaderImpl into FontLoader and make it a class

--- src/main/java/net/kemitix/fontface/FontLoader.java
+++ src/main/java/net/kemitix/fontface/FontLoader.java
@@ -1,9 +1,32 @@
 package net.kemitix.fontface;
 
-import net.kemitix.fontface.FontFace;
-
 import java.awt.*;
+import java.awt.font.TextAttribute;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
 
-public interface FontLoader {
-    Font loadFont(FontFace fontFace);
+public class FontLoader {
+
+    private static final Logger LOGGER =
+            Logger.getLogger(
+                    FontLoader.class.getName());
+
+    public Font loadFont(final FontFace fontFace) {
+        URI fontUri = fontFace.getFontLocation();
+        LOGGER.info(String.format("Loading %s", fontUri));
+        final Map<TextAttribute, Object> map = new HashMap<>();
+        //map.put(TextAttribute.LIGATURES, TextAttribute.LIGATURES_ON);
+        map.put(TextAttribute.KERNING, TextAttribute.KERNING_ON);
+        try {
+            return Font.createFont(Font.TRUETYPE_FONT, new File(fontUri))
+                    .deriveFont(map);
+        } catch (final FontFormatException | IOException e) {
+            throw new FatalFontFaceError("Font load error", e);
+        }
+    }
+
 }
